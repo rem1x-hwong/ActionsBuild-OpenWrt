ee7836c5b75e3e606b74b509a63d6da142da028d
rem1x-hwong <66793624+rem1x-hwong@users.noreply.github.com>
2022-01-31 04:23:41 +0800

    Update diy-by-packages.sh

--- ssh/diy-by-packages.sh
+++ ssh/diy-by-packages.sh
@@ -4,3 +4,7 @@ echo "src-git ssrp https://github.com/fw876/helloworld.git" >> ./feeds.conf.defa
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 git pull
+
+
+# echo "修改机器名称"
+sed -i "s/OpenWrt/R6/g" package/base-files/files/bin/config_generate
